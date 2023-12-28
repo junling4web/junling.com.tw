620750bcf89b5de3239b128083ce9251d25104bb
junling4web <143804429+junling4web@users.noreply.github.com>
2023-12-28 21:05:44 +0800

    [update] fix contact form

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,55 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const topButton = document.getElementById("topButton");
+document.addEventListener('DOMContentLoaded', function() {
+  const topButton = document.getElementById('topButton');
 
-  window.addEventListener("scroll", function() {
+  window.addEventListener('scroll', function() {
     if (window.pageYOffset > 100) {
-      topButton.style.display = "block";
+      topButton.style.display = 'block';
     } else {
-      topButton.style.display = "none";
+      topButton.style.display = 'none';
     }
   });
 
-  topButton.addEventListener("click", function() {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+  topButton.addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
   });
 
   if (window.pageYOffset > 100) {
-    topButton.style.display = "block";
+    topButton.style.display = 'block';
   } else {
-    topButton.style.display = "none";
+    topButton.style.display = 'none';
   }
 });
+
+function Submit() {
+  $('#messageModalBody').text('');
+
+  var name = $('#name').val();
+  var email = $('#email').val();
+  var message = $('#message').val();
+
+  if(name.length == 0) {
+    $('#messageModalBody').text('請輸入您的姓名。');
+    $('#messageModal').modal('show');
+    return;
+  }
+
+  if(email.length == 0) {
+    $('#messageModalBody').text('請輸入您的電子郵件。');
+    $('#messageModal').modal('show');
+    return;
+  }
+
+  if(message.length == 0) {
+    $('#messageModalBody').text('請輸入留言內容。');
+    $('#messageModal').modal('show');
+    return;
+  }
+
+  var body =
+    '留言對象：' + name + '（' + email + '）\r\n'
+    + '留言內容：\r\n' + message;
+
+  $('#body').val(body);
+
+  $('#form').submit();
+}
