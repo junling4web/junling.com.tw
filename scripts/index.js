972a125ca87dbab0027eba3bde7080534b79dc6d
junling4web <143804429+junling4web@users.noreply.github.com>
2024-04-07 03:21:25 +0800

    [update] fix mobile layout

--- scripts/index.js
+++ scripts/index.js
@@ -20,36 +20,11 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
-function Submit() {
-  $('#messageModalBody').text('');
-
-  var name = $('#name').val();
-  var email = $('#email').val();
-  var message = $('#message').val();
-
-  if(name.length == 0) {
-    $('#messageModalBody').text('請輸入您的姓名。');
-    $('#messageModal').modal('show');
-    return;
-  }
-
-  if(email.length == 0) {
-    $('#messageModalBody').text('請輸入您的電子郵件。');
-    $('#messageModal').modal('show');
-    return;
-  }
-
-  if(message.length == 0) {
-    $('#messageModalBody').text('請輸入留言內容。');
-    $('#messageModal').modal('show');
-    return;
-  }
-
-  var body =
-    '留言對象：' + name + '（' + email + '）\r\n'
-    + '留言內容：\r\n' + message;
-
-  $('#body').val(body);
-
-  $('#form').submit();
-}
+$(function(){
+  $('.navbar-nav').off('click').on('click', function(){
+    var width = $(window).width();
+    if(width < 768){
+      $('#toggleButton').trigger('click');
+    }
+  });
+});
